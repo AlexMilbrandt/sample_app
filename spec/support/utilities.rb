719cf30f07e54348213b52930d73960a85dc6048
Alex Milbrandt <amilbrandt@wisc.edu>
2014-03-30 18:00:15 -0500

    Finish Exercise 8.5.2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,6 @@
 include ApplicationHelper
 
+# requests/authentication_pages_spec.rb utilities
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
@@ -8,7 +9,20 @@ end
 
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
-    expect(page).to have_slector('div.alert.alert-error', text: message)
+    expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
 
+# requests/user_pages_spec.rb utilities 
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+def valid_credentials
+  fill_in "Name",         with: "Example Usesr"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
